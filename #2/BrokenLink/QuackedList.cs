677ce90a06fe8e514df63f1b7008a9782221264a
Vidas Vasiliauskas <vidas.vasiliauskas@danskebank.lt>
2018-01-31 17:42:46 +0200

    No2 homerwork added

---
+++ #2/BrokenLink/QuackedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrokenLink
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    // Implement linked list data structure and all missing public members of it
+    public class QuackedList
+    {
+        private Node _head; // -\_(O.o)_/-
+
+        public string Head
+        {
+            get
+            {
+                throw new NotImplementedException(); // should return first element of list
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException(); // should print all members separated by comma (CSV format) - 1,2,3,4
+        }
+
+        public string ToStringReverse()
+        {
+            throw new NotImplementedException(); // should print all members separated by comma in reverse order (figure the best and most performant way) - 4,3,2,1
+        }
+
+        public string this[int index] // indexer
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
