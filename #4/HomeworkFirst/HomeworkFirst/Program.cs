f577d0598b5af82ccb163b150ce3061def644ad4
KarolisKaj <karolis.kajenas1@gmail.com>
2018-02-15 10:26:59 +0200

    added homework for 4th session

---
+++ #4/HomeworkFirst/HomeworkFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeworkFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First:
+            // 1) Create an Interface with multiple method templates. 
+            // 2) Create two or more implementations (classes) for that interface.
+            // 3) Create a class (A) that accept these interfaces as an array into constructor.
+            // 4) Pass in two or more interfaces into class (A).
+            // 5) Call a method of class (A) which calls all passed in interfaces methods.
+                // I.E. Interface has method print(), when you call class (A) method print 
+                // it will call print method on all interfaces passed in the costructor.
+            
+            // Second:
+            // Do the same thing only using delegates (Func, Action);
+
+            // Note: this time we have manual testing by you :)
+        }
+    }
+}
